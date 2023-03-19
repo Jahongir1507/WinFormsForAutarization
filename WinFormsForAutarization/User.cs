8f17442622fe1e48fcc272126c1dbb29aa90dd2a
jahon1507 <User@USER-PC>
2023-03-19 17:14:23 +0500

    CODE RUB: Removed unnecessary usings

--- WinFormsForAutarization/User.cs
+++ WinFormsForAutarization/User.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinFormsForAutarization
-{
+﻿namespace WinFormsForAutarization {
     internal class User
     {
         public int Id { get; set; }
